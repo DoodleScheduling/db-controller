e5223a6b9aba2e046946ac3e78184e02390fc66d
tyrion85 <tyrion85@gmail.com>
2021-02-22 12:11:35 +0100

    move

--- main.go
+++ main.go
@@ -123,6 +123,15 @@ func main() {
 		options.LeaderElectionNamespace = leaderElectionNamespace
 	}
 
+	go func() {
+		setupLog.Info("Starting profiler..")
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			setupLog.Error(err, "Profiler failed to start")
+		} else {
+			setupLog.Info("Profiler started")
+		}
+	}()
+
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), options)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
@@ -196,13 +205,4 @@ func main() {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
-
-	go func() {
-		setupLog.Info("Starting profiler..")
-		if err := http.ListenAndServe(":6060", nil); err != nil {
-			setupLog.Error(err, "Profiler failed to start")
-		} else {
-			setupLog.Info("Profiler started")
-		}
-	}()
 }
