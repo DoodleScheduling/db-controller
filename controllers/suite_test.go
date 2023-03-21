bf25bc4715e7de8ef23d9ea367af0f0d46d5abca
paride5745 <paride5745@users.noreply.github.com>
2023-03-21 15:02:30 +0100

    fix path

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -62,7 +62,7 @@ var _ = BeforeSuite(func() {
 	)
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
+		CRDDirectoryPaths: []string{filepath.Join("..", "config", "base", "crd", "bases")},
 	}
 
 	var err error
